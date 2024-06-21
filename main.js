51cfd9b245f50872dfb480b59cc49b8fa7608cb1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-21 17:53:26 +0300

    Create directions to map from direction names to coord offsets

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import plan from './src/plan.js';
 import Vector from './src/vector.js';
 import Grid from './src/grid.js';
+import directions from './src/directions.js';
 
 console.log(plan);
 
@@ -14,3 +15,5 @@ var grid = new Grid(5, 5);
 console.log(grid.get(new Vector(1, 1)));
 grid.set(new Vector(1, 1), 'X');
 console.log(grid.get(new Vector(1, 1)));
+
+console.log(directions);
