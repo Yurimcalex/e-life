cad9182b9fbd7e525ee9edb3c3d32fc808d724dc
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-21 16:33:15 +0300

    Create Vector class to represent pairs of coordinates

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import './style.css'
 import plan from './src/plan.js';
+import Vector from './src/vector.js';
 
 console.log(plan);
+
+var v = new Vector(1, 2);
+var v2 = v.plus(new Vector(1, 1));
+
+console.log(v, v2);
