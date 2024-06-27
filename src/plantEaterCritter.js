98339ab6dd4afaa036c1ff05b8bd860c307ea2be
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-27 14:16:35 +0300

    refact: plant eater basic class Create separate methods for each action so that you can override any of them later.

--- src/plantEaterCritter.js
+++ src/plantEaterCritter.js
@@ -3,16 +3,43 @@ function PlantEater() {
 }
 
 PlantEater.prototype.act = function (context) {
+	// var space = context.find(' ');
+	// if (this.energy > 60 && space) {
+	// 	return {type: 'reproduce', direction: space};
+	// }
+
+	// var plant = context.find('*');
+	// if (plant) {
+	// 	return {type: 'eat', direction: plant};
+	// }
+
+	// if (space) {
+	// 	return {type: 'move', direction: space};
+	// }
+
+	var actions = ['reproduce', 'eat', 'move'];
+	for (var i = 0; i < actions.length; i += 1) {
+		var action = this[actions[i]](context);
+		if (action) return action;
+	}
+};
+
+PlantEater.prototype.reproduce = function (context) {
 	var space = context.find(' ');
 	if (this.energy > 60 && space) {
 		return {type: 'reproduce', direction: space};
 	}
+};
 
+PlantEater.prototype.eat = function (context) {
 	var plant = context.find('*');
 	if (plant) {
 		return {type: 'eat', direction: plant};
 	}
+};
 
+PlantEater.prototype.move = function (context) {
+	var space = context.find(' ');
 	if (space) {
 		return {type: 'move', direction: space};
 	}
