d7653fad4d5386e9e620664b6e46ecdc048757ee
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-27 14:25:28 +0300

    Create plant eater with eating improvement

---
+++ src/extension/critters/plantEater_1.js
@@ -0,0 +1,13 @@
+import PlantEater from '../../plantEaterCritter.js';
+
+// It eats only if it is hungry
+class PlantEater_1 extends PlantEater {
+	eat(view) {
+		let plant = view.find('*');
+		if (plant && this.energy < 80) {
+			return {type: 'eat', direction: plant};
+		}
+	}
+}
+
+export default PlantEater_1;
