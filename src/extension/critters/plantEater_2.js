213453849dbdd4d2fc6f1f504f12a07338a4a540
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-28 15:47:57 +0300

    Create plant eater with another moving algorithm

---
+++ src/extension/critters/plantEater_2.js
@@ -0,0 +1,52 @@
+import PlantEater from '../../plantEaterCritter.js';
+import { randomElement } from '../utils.js';
+
+// If it is hungry it starts looking for food
+//When it gets obstacles it change its direction according to the following rules:
+//If it was moving horizontally it starts moving vertically in random direction.
+//If it was moving vertically it starts moving horizontally in random direction.
+//If by this time there is no direction the direction chose randomly vertical or horizontal.
+
+class PlantEater_2 extends PlantEater {
+	dir = 'w';
+
+	move(view) {
+		if (this.energy < 20) {
+			let horizontal = ['w', 'e'];
+			let vertical = ['n', 's'];
+			let nextDir;
+
+			if (view.look(this.dir) === ' ') {
+				nextDir = this.dir;
+			}
+
+			if (!nextDir && horizontal.includes(this.dir)) {
+				nextDir = randomElement(vertical);
+
+				if (view.look(nextDir) !== ' ') {
+					nextDir = null;
+				}
+			}
+
+			if (!nextDir && vertical.includes(this.dir)) {
+				nextDir = randomElement(horizontal);
+
+				if (view.look(nextDir) !== ' ') {
+					nextDir = null;
+				}
+			}
+
+			
+			if (!nextDir) nextDir = super.move(view).direction;
+
+			this.dir = nextDir;
+			
+			return {type: 'move', direction: nextDir};
+		
+		} else {
+			return super.move(view);
+		}
+	}
+}
+
+export default PlantEater_2;
