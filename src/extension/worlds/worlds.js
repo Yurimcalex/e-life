1a27b4bb5c94eacae1bf9d966cbb4c2571bad98d
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-04 15:01:07 +0300

    refact: Create world #5

--- src/extension/worlds/worlds.js
+++ src/extension/worlds/worlds.js
@@ -1,75 +1,8 @@
-import FirstPlan from '../../plan.js';
-import valleyPlan from '../../valleyPlan.js';
-import { emptyPlan } from '../plans/test.js';
-
-import World from '../../world.js';
-import LifelikeWorld from '../../lifeLikeWorld.js';
-
-import Wall from '../../wall.js';
-import BouncingCritter from '../../bouncingCritter.js';
-import WallFollower from '../../wallFollowerCritter.js';
-import Plant from '../../plantCritter.js';
-import PlantEater from '../../plantEaterCritter.js';
-//import SavyEater from '../critters/plantEater_1.js';
-//import SavyMovingEater from '../critters/plantEater_2.js';
-
-import { replaceChar, randomPlaceChar } from '../utils.js';
-
-
 import world1 from './world1.js';
 import world2 from './world2.js';
 import world3 from './world3.js';
 import world4 from './world4.js';
+import world5 from './world5.js';
 
 
-
-let worlds = [ world1, world2, world3, world4 ];
-
-// let worlds = [
-// 	{
-// 		world: World,
-// 		plan: [...FirstPlan],
-// 		legend: {
-// 			'#': Wall,
-// 			'o': BouncingCritter,
-// 			'w': WallFollower
-// 		}
-// 	},
-// 	{
-// 		world: LifelikeWorld,
-// 		plan: [...valleyPlan],
-// 		legend: {
-// 			'#': Wall,
-// 			'0': PlantEater,
-// 			'*': Plant
-// 		}
-// 	},
-// 	{
-// 		world: LifelikeWorld,
-// 		plan: replaceChar([...valleyPlan], '0', 'D'),
-// 		legend: {
-// 			'#': Wall,
-// 			'D': SavyEater,
-// 			'*': Plant
-// 		}
-// 	},
-// 	{
-// 		world: LifelikeWorld,
-// 		plan: randomPlaceChar([...emptyPlan], 'E'),
-// 		legend: {
-// 			'#': Wall,
-// 			'E': SavyMovingEater
-// 		}
-// 	},
-// 	{
-// 		world: LifelikeWorld,
-// 		plan: replaceChar([...valleyPlan], '0', 'E'),
-// 		legend: {
-// 			'#': Wall,
-// 			'E': SavyMovingEater,
-// 			'*': Plant
-// 		}
-// 	}
-// ];
-
-export default worlds;
+export default [ world1, world2, world3, world4, world5 ];
