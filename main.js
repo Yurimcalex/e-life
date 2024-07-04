8922c8b5938c1d7ca92249e4d08efb3aa021a53a
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-04 12:39:26 +0300

    refact: Update main

--- main.js
+++ main.js
@@ -1,67 +1,42 @@
 import './style.css'
 import worlds from './src/extension/worlds/worlds.js';
-import { saveItem, getItem } from './src/extension/storage.js';
+import Controller from './src/extension/controller.js';
 
 
-var world;
-var wInd = getItem('worldIndex') || 0;
-var app = document.getElementById('world-str');
+const view = document.getElementById('world-str');
+const controller = new Controller(
+	worlds,
+	createWorld,
+	view,
+	updateView,
+	run
+);
+
+controller.init();
+createSelectOptions(worlds);
 
 
-createWorld(worlds[wInd]);
-app.innerHTML = world.toString();
+function updateView(world, view) {
+	view.innerHTML = world.toString();
+}
 
+function createWorld(options) {
+	const {world, plan, legend} = options;
+	return new world(plan, legend);
+}
 
-var timer;
-startBtn.onclick = function () {
-	if (!timer) render();
-};
-stopBtn.onclick = function () {
-	if (timer) {
-		clearInterval(timer);
-		timer = null;
-	}
-};
-restartBtn.onclick = function () {
-	if (timer) stopBtn.click();
-	createWorld(worlds[wInd]);
-	app.innerHTML = world.toString();
-	startBtn.click();
-};
-selectWorld.onchange = function () {
-	wInd = parseInt(this.value);
-	if (timer) stopBtn.click();
-	createWorld(worlds[wInd]);
-	app.innerHTML = world.toString();
-	saveItem('worldIndex', wInd);
-};
-
-createSelectOptions();
-selectWorld.selectedIndex = wInd;
-
-
-function createSelectOptions() {
-	for (var i = 0; i < worlds.length; i += 1) {
-		var option = document.createElement('option');
+function createSelectOptions(worlds) {
+	worlds.forEach((_w, i) => {
+		const option = document.createElement('option');
 		option.value = i;
 		option.innerHTML = 'world' + ' ' + i;
 		selectWorld.appendChild(option);
-	}
+	});
 }
 
-
-function createWorld(options) {
-	let w = options.world;
-	let plan = options.plan;
-	var legend = options.legend;
-
-	world = new w(plan, legend);
-}
-
-
-function render() {
-	timer = setInterval(() => {
-	  app.innerHTML = world.toString();
+function run(world, view) {
+	return setInterval(() => {
+	  updateView(world, view);
 	  world.turn();
 	}, 500);
 }
