e863d3a99b89bd8aa36aae786b36addeabec58ea
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-05 18:37:55 +0300

    Create paint critters feature

--- src/extension/table.js
+++ src/extension/table.js
@@ -3,6 +3,15 @@ export default class Table {
 		this.container = document.querySelector('.view');
 		this.table = null;
 		this.data = '';
+		this.coloredCells = null;
+		this.colors = {
+			'#': 'black',
+			'o': 'gray',
+			'*': 'green',
+			'0': 'brown',
+			'D': 'brown',
+			'E': 'brown'
+		};
 	}
 
 	createTable(data) {
@@ -21,16 +30,22 @@ export default class Table {
 		this.table = table;
 		this.data = data;
 		this.container.append(table);
+		this._paint();
 	}
 
 	updateTable(data) {
 		const cellsToUpdate = this._getCellsToUpdate(this.data, data);
 		const rows = this.table.rows;
+		const cellsToColor = [];
 		cellsToUpdate.forEach(cd => {
 			const {row, col, text} = cd;
-			rows[row].cells[col].textContent = text;
+			const td = rows[row].cells[col];
+			td.textContent = text;
+			cellsToColor.push(td);
 		});
 		this.data = data;
+		this.coloredCells.forEach(td => td.style.color = '');
+		this._paint(cellsToColor);
 	}
 
 	_getCellsToUpdate(prevData, data) {
@@ -58,11 +73,40 @@ export default class Table {
 		return cellsToUpdate;
 	}
 
+	_paint(cellsToColor) {
+		const coloredCells = [];
+		const colors = this.colors;
+		if (!cellsToColor) {
+			const tds = this.table.querySelectorAll('td');
+			Array.from(tds).forEach(td => {
+				const key = td.textContent;
+				if (key in colors) {
+					td.style.color = colors[key];
+					coloredCells.push(td);
+				}
+			});
+			
+		} else {
+			cellsToColor.forEach(td => {
+				const key = td.textContent;
+				if (key in colors) {
+					td.style.color = colors[key];
+					coloredCells.push(td);
+				}
+			});
+		}
+		this.coloredCells = coloredCells;
+	}
+
+	remove() {
+		this.table.remove();
+	}
+
 	render(data) {
 		if (!this.table) {
 			this.createTable(data);
 		} else {
 			this.updateTable(data);
-		}	
+		}
 	}
 }
