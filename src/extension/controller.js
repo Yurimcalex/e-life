c9d1c1ab79f53fe00be90073fd940797d6e80136
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-05 11:12:27 +0300

    refact: Update Controller class

--- src/extension/controller.js
+++ src/extension/controller.js
@@ -1,36 +1,28 @@
 import { saveItem, getItem } from './storage.js';
 
 export default class Controller {
-	constructor(worlds, createWorld, view, updateView, run) {
+	constructor(update, run) {
+		this.update = update;
+		this.run = run;
 		this.startBtn = document.getElementById('startBtn');
 		this.stopBtn = document.getElementById('stopBtn');
 		this.restartBtn = document.getElementById('restartBtn');
 		this.selectBtn = document.getElementById('selectWorld');
-
-		const ind = getItem('worldIndex') || 0;
-		this.worlds = worlds;
-		this.world = createWorld( worlds[ind] );
-		this.ind = ind
-
-		this.createWorld = createWorld;
-		this.updateView = updateView;
-		this.view = view;
-		this.run = run;
-		
 		this.timer = null;
+		this.worldN =  getItem('worldIndex') || 0;
 	}
 
 	init() {
-		this.updateView(this.world, this.view);
-		this.startBtn.onclick = this.start.bind(this);
-		this.stopBtn.onclick = this.stop.bind(this);
-		this.restartBtn.onclick = this.restart.bind(this);
-		this.selectBtn.addEventListener('change', (e) => this.selectWorld(e));
+		this.startBtn.onclick = () => this.start();
+		this.stopBtn.onclick = () => this.stop();
+		this.restartBtn.onclick = () => this.restart();
+		this.selectBtn.addEventListener('change', (e) => 
+			this.selectWorld(e.target.value));
 	}
 
 	start() {
 		if (!this.timer) {
-			this.timer = this.run(this.world, this.view);
+			this.timer = this.run();
 		}
 	}
 
@@ -45,19 +37,16 @@ export default class Controller {
 		if (this.timer) {
 			this.stopBtn.click();
 		}
-		this.world = this.createWorld(this.worlds[this.ind]);
-		this.updateView(this.world, this.view);
+		this.update(this.worldN);
 		this.startBtn.click();
 	}
 
-	selectWorld(e) {
-		const ind = parseInt(e.target.value);
+	selectWorld(worldN, update) {
 		if (this.timer) {
 			this.stopBtn.click();
 		}
-		this.world = this.createWorld(this.worlds[ind]);
-		this.ind = ind;
-		this.updateView(this.world, this.view);
-		saveItem('worldIndex', ind);
+		this.update(worldN);
+		this.worldN = worldN;
+		saveItem('worldIndex', worldN);
 	}
-}
+};
