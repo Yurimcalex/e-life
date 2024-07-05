d6f441454b67e8a4f5c843b44a883c1f3674b1ea
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-05 17:45:27 +0300

    refact: Create class Table to display data

---
+++ src/extension/table.js
@@ -0,0 +1,68 @@
+export default class Table {
+	constructor() {
+		this.container = document.querySelector('.view');
+		this.table = null;
+		this.data = '';
+	}
+
+	createTable(data) {
+		const table = document.createElement('table');
+		table.className = 'table';
+		table.innerHTML = `
+			${data.split('\n').reduce((acc, row) => {
+				return acc + `<tr>
+					${row.split('').reduce((acc, cell) => {
+						return acc + `<td>${cell}</td>`;
+					}, '')}
+				</tr>` 
+			}, '')}
+		`;
+		
+		this.table = table;
+		this.data = data;
+		this.container.append(table);
+	}
+
+	updateTable(data) {
+		const cellsToUpdate = this._getCellsToUpdate(this.data, data);
+		const rows = this.table.rows;
+		cellsToUpdate.forEach(cd => {
+			const {row, col, text} = cd;
+			rows[row].cells[col].textContent = text;
+		});
+		this.data = data;
+	}
+
+	_getCellsToUpdate(prevData, data) {
+		let rowInd = 0;
+		let colInd = 0;
+		let cellsToUpdate = [];
+
+		for (let i = 0; i < data.length; i += 1) {
+			if (data[i] !== prevData[i]) {
+				cellsToUpdate.push({
+					row: rowInd,
+					col: colInd,
+					text: data[i]
+				});
+			}
+
+			colInd += 1;
+
+			if (data[i] === '\n') {
+				rowInd += 1;
+				colInd = 0;
+			}
+		}
+
+		return cellsToUpdate;
+	}
+
+	render(data) {
+		if (!this.table) {
+			this.createTable(data);
+		} else {
+			this.updateTable(data);
+		}	
+	}
+}
