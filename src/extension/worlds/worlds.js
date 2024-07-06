e783c0bf7d79ca472d22c37d25fd5d289d5aacc9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-06 19:57:39 +0300

    Add world #6

--- src/extension/worlds/worlds.js
+++ src/extension/worlds/worlds.js
@@ -3,6 +3,7 @@ import world2 from './world2.js';
 import world3 from './world3.js';
 import world4 from './world4.js';
 import world5 from './world5.js';
+import world6 from './world6.js';
 
 
-export default [ world1, world2, world3, world4, world5 ];
+export default [ world1, world2, world3, world4, world5, world6 ];
