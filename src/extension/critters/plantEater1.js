5b3cdda263841dd88b1e9203bd5d8e536e8a0c3d
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-08 17:28:26 +0300

    Update plant eater 1 description

--- src/extension/critters/plantEater1.js
+++ src/extension/critters/plantEater1.js
@@ -1,7 +1,14 @@
 import PlantEater from '../../plantEaterCritter.js';
 
-const description = 'It inherits all the props from Plant Eater and defines its own eat action. ' + 
-'It eats only if it is hungry - the energy level is less than 80 points.';
+
+const description = `
+	It is a plant eater.
+	It can move, eat and reproduce.
+	It has 20 energy points on start.
+	<em>Eat:</em> It eats plants. It eats only if it is hungry - the energy level is less than 80 points.
+	<em>Move:</em> Tt moves in random direction.
+	<em>Reproduce:</em> It gets reproduced if it has more than 60 energy points and there is a free spot.
+`;
 
 class PlantEater1 extends PlantEater {
 	eat(view) {
