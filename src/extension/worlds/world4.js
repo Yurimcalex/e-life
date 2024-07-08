d09f201bd5b7ad2afd910efc8c1122d447383b93
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-08 12:01:29 +0300

    Test Plant Eater 4

--- src/extension/worlds/world4.js
+++ src/extension/worlds/world4.js
@@ -2,13 +2,15 @@ import { emptyPlan } from '../plans/test.js';
 import world from '../../lifeLikeWorld.js';
 import Wall from '../../wall.js';
 import PlantEater from '../critters/plantEater2.js';
+import PlantEater4 from '../critters/plantEater4.js';
 import { randomPlaceChar } from '../utils.js';
 
 export default {
-	plan: randomPlaceChar([...emptyPlan], 'E'),
+	plan: randomPlaceChar([...emptyPlan], 'e'),
 	world: world,
 	legend: {
 		'#': Wall,
-		'E': PlantEater
+		// 'E': PlantEater,
+		'e': PlantEater4
 	}
 };
