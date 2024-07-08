86d0c384fa8ef0169c9aba208ecfc9f281535ff2
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-08 14:25:06 +0300

    Create a world with plant eater 5

--- src/extension/worlds/worlds.js
+++ src/extension/worlds/worlds.js
@@ -5,6 +5,6 @@ import world4 from './world4.js';
 import world5 from './world5.js';
 import world6 from './world6.js';
 import world7 from './world7.js';
+import world8 from './world8.js';
 
-
-export default [ world1, world4, world2, world3, world5, world6, world7 ];
+export default [ world1, world4, world2, world3, world5, world6, world7, world8 ];
