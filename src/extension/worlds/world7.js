9e7803bf40f2334c456ec869d7b404342e8f4407
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-08 12:01:57 +0300

    Create a world with plant eater 4

---
+++ src/extension/worlds/world7.js
@@ -0,0 +1,16 @@
+import plan from '../../valleyPlan.js';
+import world from '../../lifeLikeWorld.js';
+import Wall from '../../wall.js';
+import PlantEater from '../critters/plantEater4.js';
+import Plant from '../../plantCritter.js';
+import { replaceChar } from '../utils.js';
+
+export default {
+	plan: replaceChar([...plan], '0', 'E'),
+	world,
+	legend: {
+		'#': Wall,
+		'E': PlantEater,
+		'*': Plant
+	}
+};
--- src/extension/worlds/worlds.js
+++ src/extension/worlds/worlds.js
@@ -4,6 +4,7 @@ import world3 from './world3.js';
 import world4 from './world4.js';
 import world5 from './world5.js';
 import world6 from './world6.js';
+import world7 from './world7.js';
 
 
-export default [ world1, world2, world3, world4, world5, world6 ];
+export default [ world1, world4, world2, world3, world5, world6, world7 ];
