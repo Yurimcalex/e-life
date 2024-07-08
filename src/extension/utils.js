f2919ad6b8cac301a8481417ca8c69734e0e4bf5
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-08 14:23:22 +0300

    Export randomInteger func

--- src/extension/utils.js
+++ src/extension/utils.js
@@ -34,7 +34,7 @@ export function randomElement(array) {
 }
 
 
-function randomInteger(min, max) {
+export function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
